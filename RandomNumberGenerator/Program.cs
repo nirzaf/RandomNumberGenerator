a9b9d0ee9772d3b5b0b4ec80039b4a1e0aefbfaf
MFM_Fazrin <mmfazrin@phcc.gov.qa>
2022-07-27 11:33:50 +0300

    Initial commit

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RandomNumberGenerator;
+
+for(int i=0; i<10; i++)
+{
+    Console.WriteLine("Request Number :  " + RandomRequestNumber.Generate());
+}
+
+Console.ReadLine();
---
+++ RandomNumberGenerator/RandomRequestNumber.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace RandomNumberGenerator;
+
+public static class RandomRequestNumber
+{
+    public static string Generate()
+    {
+        StringBuilder builder = new();
+        Random random = new();
+        for (int i = 0; i < 4; i++)
+        {
+            var ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+            builder.Append(ch);
+        }
+
+        var num = random.Next(100, 1000);
+
+        if (num % 2 == 0)
+        {
+            builder.Insert(0, num);
+        }
+        else
+        {
+            builder.Append(num);
+        }
+
+        num = DateTime.Now.Millisecond;
+
+        if (num % 2 == 0)
+        {
+            builder.Insert(0, DateTime.Now.Second);
+        }
+        else
+        {
+            builder.Append(DateTime.Now.Second);
+        }
+
+        return builder.ToString();
+    }
+}
